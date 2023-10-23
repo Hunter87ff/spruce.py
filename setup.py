c662f30c1d203c0b42f366ab34c5f40e90456279
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-23 15:57:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
         'Programming Language :: Python',
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.10",
