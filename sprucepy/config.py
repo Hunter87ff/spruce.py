11f856c4edffd5ee6c5867723a2386de84c2a493
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-31 19:52:47 +0530

    Update config.py

--- sprucepy/config.py
+++ sprucepy/config.py
@@ -1,1 +1,16 @@
+matches = [
+    {"query": ["usd to inr", " usd to inr", "usd to inr", " usd inr", "usd inr"], "frm": "usd", "to": "inr"},
+    {"query": ["inr to usd", " inr to usd", "inr to usd", " inr usd", "inr usd"], "frm": "inr", "to": "usd"},
+    {"query": ["usd to eur", " usd to eur", "usd to eur", " usd eur", "usd eur"], "frm": "usd", "to": "eur"},
+    {"query": ["eur to usd", " eur to usd", "eur to usd", " eur usd", "eur usd"], "frm": "eur", "to": "usd"},
+    {"query": ["usd to gbp", " usd to gbp", "usd to gbp", " usd gbp", "usd gbp"], "frm": "usd", "to": "gbp"},
+    {"query": ["gbp to usd", " gbp to usd", "gbp to usd", " gbp usd", "gbp usd"], "frm": "gbp", "to": "usd"},
+    {"query": ["usd to aud", " usd to aud", "usd to aud", " usd aud", "usd aud"], "frm": "usd", "to": "aud"},
+    {"query": ["aud to usd", " aud to usd", "aud to usd", " aud usd", "aud usd"], "frm": "aud", "to": "usd"},
+    {"query": ["usd to cad", " usd to cad", "usd to cad", " usd cad", "usd cad"], "frm": "usd", "to": "cad"},
+    {"query": ["cad to usd", " cad to usd", "cad to usd", " cad usd", "cad usd"], "frm": "cad", "to": "usd"},
+    {"query": ["usd to chf", " usd to chf", "usd to chf", " usd chf", "usd chf"], "frm": "usd", "to": "chf"},
+    {"query": ["usd to pkr", " usd to pkr", "usd to pkr", " usd pkr", "usd pkr"], "frm": "usd", "to": "pkr"},
+]
+
 print("Hunter87")
